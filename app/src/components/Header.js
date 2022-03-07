04ade69fd5349eb05b698ff6e7b366536466693c
jorge garate <jorgeluis.garate@gmail.com>
2022-03-06 22:33:51 -0300

    nuevos datos navbar

--- app/src/components/Header.js
+++ app/src/components/Header.js
@@ -1,10 +1,12 @@
 import Navbar from "./NavBar";
+import IconsNavbar from "./IconsNavbar";
 import logo from '../logo.svg';
 function Header() {
     return (
         <header>
             <img src={logo} className="" alt="logo" />
             <Navbar />
+            <IconsNavbar />
         </header>
     );
 }
---
+++ app/src/components/IconsNavbar.js
@@ -0,0 +1,13 @@
+function IconsNavbar() {
+    return (
+        <div>
+            <ul>
+                <li>1</li>
+                <li>2</li>
+                <li>3</li>
+            </ul>
+        </div>
+    )
+
+}
+export default IconsNavbar
